e683367a8f28dd87e8c12f775a21462880f133ed
열일 <isawthat77@naver.com>
2022-01-13 19:04:20 +0900

    검색 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import './App.css';
+import styles from './App.module.css';
 import React, {useEffect, useState} from 'react';
 import VideoList from './components/video_list/video_list';
+import VideoSearch from './components/video_search/video_search';
 
 function App() {
   const [videos, setVideos]=useState([]);
-  useEffect(() => {
+  const search = query =>{
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyAJ5SVwT7RxfiEzkIR0E7TmFYJ-a5LDzD8`, requestOptions)
+      .then(response => response.json())
+      .then(result => result.items.map(item => ({...item, id:item.id.videoId})))
+      .then(items => setVideos(items))
+      .catch(error => console.log('error', error));
+  }
+
+ useEffect(() => {
     const requestOptions = {
       method: 'GET',
       redirect: 'follow'
@@ -17,7 +31,10 @@ function App() {
   }, []);
 
   return (
+    <div className={styles.app}>
+    <VideoSearch onSearch={search}></VideoSearch>
     <VideoList videos={videos}></VideoList>
+    </div>
   );
 }
 
