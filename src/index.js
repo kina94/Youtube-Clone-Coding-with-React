85cd5fd86b3649758c2aeaa5cab75128eb6c76a2
열일 <isawthat77@naver.com>
2022-01-19 21:58:25 +0900

    hide youtube api key

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import './index.css';
 import App from './App';
 import Youtube from './components/service/youtube';
 
-const youtube=new Youtube(process.env.REACT_APP_YOUTUBE_API_KEY);
+const httpClient = axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: { key: process.env.REACT_APP_YOUTUBE_API_KEY },
+});
+const youtube = new Youtube(httpClient);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App youtube={youtube}/>
+    <App youtube={youtube} />
   </React.StrictMode>,
   document.getElementById('root')
 );
